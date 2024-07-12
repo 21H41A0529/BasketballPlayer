fcf1cc832df2f3dca77ea40815a61e72cfa9e003
21H41A0529 <lakshmikumpatla25@gmail.com>
2024-07-12 11:50:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BasketballPlayerCard from './components/BasketballPlayerCard';
+import "./App.css"
 
-function App() {
+const App = () => {
+  const player = {
+    name: "LeBron James",
+    image: "https://res.cloudinary.com/dpf3dhf45/image/upload/v1720758365/uh1ohp7ly6untk1xgdk9.jpg",
+    position: "Forward",
+    stats: {
+      pointsPerGame: 25.4,
+      assistsPerGame: 7.1,
+      reboundsPerGame: 10.5
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Basketball Player Card</h1>
+      <BasketballPlayerCard
+        name={player.name}
+        image={player.image}
+        position={player.position}
+        stats={player.stats}
+      />
     </div>
   );
-}
+};
 
 export default App;
